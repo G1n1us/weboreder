5cb22068e8374f926fe40d5f99608bb9e1a53734
G1n1us <Aleksei.rusin@tptlive.ee>
2020-10-26 15:45:54 +0200

    update

--- send.php
+++ send.php
@@ -1,4 +1,5 @@
 <?php
+/*
 if(mail("lait@mail.ru", "My Subject", "Line 1\nLine 2\nLine 3")) echo "message send";
 else echo "message not send";
 
@@ -14,8 +15,33 @@ if (isset($_POST["send"]))
 
 	mail($to, $subject, $msg, $headerss);
 	print "<script>alert(\"Сообщение успешно отправлено!\");window.location = window.location.href</script>";
+}*/
+?>
+
+
+<?php
+if($_POST){
+  $email = $_POST['email'];
+  $name = $_POST['phone'];
+  $object = $_POST['copy'];
+  $message = $_POST['double'];
+
+  $headers = "MIME-Version: 1.0\r\n";
+  $headers .= "Content-type: text/plain; charset=iso-8859-1\r\n";
+  $headers .= "From: $name <$email>\r\nReply-to : $name <$email>\nX-Mailer:PHP";
+
+  $subject="$objet";
+  $destinataire="teenused.peter@byroomaailm.ee";
+  $body="$message";
+
+  if(mail($destinataire,$subject,$body,$headers)) {
+    $response['status'] = 'success';
+    $response['msg'] = 'your mail is sent';
+  } else {
+    $response['status'] = 'error';
+    $response['msg'] = 'Something went wrong';
+  }
+
+  echo json_encode($response);
 }
 ?>
-<?php
-print mail("name@my.ru","header","text");
-?> 
