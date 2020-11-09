6008c6a45b167bd5ae63d2e232935b7037b795a3
G1n1us <aleksei.rusin@tptlive.ee>
2020-11-09 21:39:03 +0200

    Send_form

--- send.php
+++ send.php
@@ -2,7 +2,7 @@
 if($_POST){
   $email	 = $_POST['email'];
   $phone	 = $_POST['phone'];
-  //$copynr	 = $_POST['copynr'];
+  $copy	     = $_POST['copy'];
   $double	 = $_POST['double'];
   $color 	 = $_POST['color'];
   $link		 = $_POST['link'];
@@ -10,34 +10,27 @@ if($_POST){
 
   $headers = "MIME-Version: 1.0\r\n";
   $headers .= "Content-type: text/plain; charset=iso-8859-1\r\n";
-  $headers .= "$email /n $phone /n $double /n $color /n $link /n $comment";
-  $message = 
-  " 
-  <html>
-<head>
-<title>HTML email</title>
-</head>
-<body>
-<p>This email contains HTML Tags!</p>
-<table>
-<tr>
-<th>Firstname</th>
-<th>Lastname</th>
-</tr>
-<tr>
-<td>John</td>
-<td>Doe</td>
-</tr>
-</table>
-</body>
-</html>
-  ";
+  $headers .= "Tellija: $email 
+  Telefoni number: $phone";
+  $message =" Kliendi e-mail: $email
+  Kliendi telefon: $phone
+  ---------------------------------
+  Printimine: 
+  1) Koopia kogus: $copy
+  2) 1-poolne või 2-poolne: $double
+  3) Värviline või Must/valge: $color
+  4) We-transfer link: $link
+  5) Lisa info: $comment";
   $subject = "E-tellimine printimine teenus";
-  $to="aleksei.rusin@byroomaailm.ee, byroomaailm.teenuse@gmail.com";
+  //$from="E-printimine@byroomaailm.ee";
+  $to="aleksei.rusin@byroomaailm.ee, byroomaailm.teenuse@gmail.com, aleksei.rusin@tptlive.ee";
 
-  if(mail($to,$subject,$headers)) {
+  if(mail($to,$subject,$message,$headers)) {
     $response['status'] = 'success';
     $response['msg'] = 'your mail is sent';
+   if (isset($_SERVER["HTTP_REFERER"])) {
+        header("Location: " . $_SERVER["HTTP_REFERER"]);
+    }
   } else {
     $response['status'] = 'error';
     $response['msg'] = 'Something went wrong';
