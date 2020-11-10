a68f211634792e1b24cc74af5a942d827f8232e6
G1n1us <Aleksei.rusin@tptlive.ee>
2020-11-10 16:32:08 +0200

    Logo del and button

--- thankyou.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-echo "
-<div class='container'>
-<img src='pic/bm_logo.png' alt='B&uuml;roomaailm logo' class='center' width='' height='auto'>
-	<a class='btn btn-primary' href='index.html' role='button'>Esitada uue tellimus</a>
-</div>	";
-//<a href=index.html>Esitada uue tellimus</a>
- ?>
